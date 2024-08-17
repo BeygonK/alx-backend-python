1198fcbd3a3762b81985f0b2067b17e92c9aee75
BeygonK <kipkoechbeygon@gmail.com>
2024-08-17 13:24:32 +0300

    duck typing

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,10 +2,9 @@
 """Duck typing"""
 
 
-from typing import List, Tuple
+from typing import List, Tuple, Iterable, Sequence
 
-
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """summary
 
     Args:
