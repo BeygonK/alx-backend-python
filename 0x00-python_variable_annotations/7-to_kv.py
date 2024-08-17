c6e125614dae7d8b8763b039361b337ee6eb1c02
BeygonK <kipkoechbeygon@gmail.com>
2024-08-17 13:04:50 +0300

    to kv

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""module
+"""
+
+
+from typing import Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple:
+    """A function to return a tuple
+
+    Args:
+        k (str): The first argument
+        v (Union[int, float]): The second argument
+
+    Returns:
+        tuple: results
+    """
+    return (k, float(v ** 2))
