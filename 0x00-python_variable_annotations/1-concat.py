14c0e23c67ff5561c5d88fb9452037d4f8468d65
BeygonK <kipkoechbeygon@gmail.com>
2024-08-17 11:37:31 +0300

    concat strings

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -15,5 +15,5 @@ def concat(str1: str, str2: str) -> str:
     Returns:
         str: result string
     """
-    result = "{} {}".format(str1, str2)
+    result = str1 + "" + str2
     return result
