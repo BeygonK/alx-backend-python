0f9c42173bd8b657359579746e2cc6fdc672424c
BeygonK <kipkoechbeygon@gmail.com>
2024-08-29 10:58:10 +0300

    parameterized a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,7 +7,19 @@ from typing import Any, Sequence, Mapping
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Tests the access_nested_map function"""
+    @parameterized.expand([
+        ({}, ("a",), "'a'"),
+        ({"a": 1}, ("a", "b"), "'b'"),
+        ])
+    def test_access_nested_map_exception(self,
+                                         nested_map: Mapping,
+                                         path: Sequence,
+                                         expected_message: str) -> None:
+        """Test access_nested_map with exception"""
+        with self.assertRaises(Exception) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(e.exception), expected_message)
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
