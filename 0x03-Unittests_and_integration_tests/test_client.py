1b11c04fed28f520c91db2eee1712335c2ea8e47
BeygonK <kipkoechbeygon@gmail.com>
2024-08-31 09:57:00 +0300

    test client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -23,5 +23,19 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, {"key": "value"})
 
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),    
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test that the given license key is  in the list of licenses"""
+        client = GithubOrgClient('test_org')
+        with patch.object(GithubOrgClient,
+                          'has_license',
+                          return_value=expected):
+            result = client.has_license(repo, license_key)
+            self.assertEqual(result, expected)
+
+
 if __name__ == "__main__":
     unittest.main()
